fbe6a4f88d0709df0255e81303d41ba92ff46118
Brian Coffey <56007431+brianpcoffey@users.noreply.github.com>
2023-03-03 11:39:19 -0800

    Reformatted TwoSum Solution

--- hashmap/TwoSum.cpp
+++ hashmap/TwoSum.cpp
@@ -18,14 +18,9 @@ vector<int> TwoSum(vector<int>& nums, int target) {
 
 //compile with "g++ -std=c++11 TwoSum.cpp"
 int main() {
-    vector<int> x = {2,7,11,15};
-    vector<int> x1 = {0,1};
-    vector<int> y = {3,2,4};
-    vector<int> y1 = {1,2};
-    vector<int> z = {3,3};
-    vector<int> z1 = {0,1};
-    (TwoSum(x,9) == x1) ? cout << "TEST #1 PASSED" << endl : cout << "TEST #1 FAILED" << endl;
-    (TwoSum(y,6) == y1) ? cout << "TEST #2 PASSED" << endl : cout << "TEST #2 FAILED" << endl;
-    (TwoSum(z,6) == z1) ? cout << "TEST #3 PASSED" << endl : cout << "TEST #3 FAILED" << endl;
+    vector<int> x = {2,7,11,15}, xs = {0,1}, y = {3,2,4}, ys = {1,2}, z = {3,3}, zs = {0,1};
+    (TwoSum(x,9) == xs) ? cout << "TEST #1 PASSED" << endl : cout << "TEST #1 FAILED" << endl;
+    (TwoSum(y,6) == ys) ? cout << "TEST #2 PASSED" << endl : cout << "TEST #2 FAILED" << endl;
+    (TwoSum(z,6) == zs) ? cout << "TEST #3 PASSED" << endl : cout << "TEST #3 FAILED" << endl;
     return 0;
 }
